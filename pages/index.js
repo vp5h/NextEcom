6a65a7a0ea89c041616816d225d2d67b342848f1
Pravesh Vyas <praveshvyas.18@gmail.com>
2021-10-01 21:58:47 +0530

    prod listing

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { getData } from '../utils/fetchdata';
+import Head from 'next/head'
+import ProductItem from '../components/product/ProductItem';
 
-export default function index() {
-  return <div>Home</div>;
+const Home = (props) => {
+  const [products, setProducts] = useState(props.products);
+  console.log(products);
+  
+  const handleCheck = (id) => {
+    products.forEach(product => {
+      if(product._id === id) product.checked = !product.checked
+    })
+    setProducts([...products])
+  }
+  
+  
+  return( 
+  
+  <div className="products">
+    <Head>
+      <title>Home Page</title>
+    </Head>
+   {
+     products.length === 0
+     ? <h2>No produts</h2>
+      :products.map(product => (
+        <ProductItem key={product._id} product={product} handleCheck={handleCheck} />
+      ))
+    }
+   
+    </div>);
+};
+
+export async function getServerSideProps() {
+  // const page = query.page || 1
+  // const category = query.category || 'all'
+  // const sort = query.sort || ''
+  // const search = query.search || 'all'
+
+  const res = await getData('product');
+  // server side rendering
+  return {
+    props: {
+      products: res.products,
+      result: res.result,
+    }, // will be passed to the page component as props
+  };
 }
+
+export default Home;
