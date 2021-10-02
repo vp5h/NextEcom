a0ebd60d4d434768de721a973b72ca57fa1dd6c4
Pravesh Vyas <praveshvyas.18@gmail.com>
2021-10-02 21:43:27 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    BASE_URL: 'http://localhost:3000',
+    MONGODB_URL:
+      'mongodb+srv://pravesh:project12@cluster0.ljowm.mongodb.net/Cluster0?retryWrites=true&w=majority',
+    ACCESS_TOKEN_SECRET: 'JJcF4sQQ_7$Hvg$7Z(~uL%3=UFT;wJHa?xw<ZycTC2wAH#MVt:',
+    REFRESH_TOKEN_SECRET:
+      'bBDTv7KZM=69uC6^=7hK6f@^-G8qvFN^LhCeLaJfq-QjFdX^S%v9585T%qHU6SKa&kp%JfW6Q@Q==5NY',
+    PAYPAL_CLIENT_ID:
+      'AZRVYCppxsjipnhhx6fmMREi0q0WlDnfLV-S5Qw7UmpoClmL6tF4TlaoLmWrfxFoLcXrwhmXCxOoj3--',
+    CLOUD_UPDATE_PRESET: 'nextjs_store',
+    CLOUD_NAME: 'nextecom',
+    CLOUD_API: 'https://api.cloudinary.com/v1_1/nextecom/image/upload',
+  },
 };
