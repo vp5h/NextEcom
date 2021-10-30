7ddedca694eef27fceacee6daef652528265e99a
Pravesh Vyas <praveshvyas.18@gmail.com>
2021-10-30 17:49:23 +0530

    paypal

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ class Mydocument extends Document {
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
           <script src="https://kit.fontawesome.com/a076d05399.js"></script>
           <script
-            src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`}
+            src={`https://www.paypal.com/sdk/js?client-id=${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}`}
           ></script>
         </Head>
         <body>
